8001464851b5e2b56755aabd8d1df206bb2ec77b
unknown <lucusapp@gmail.com>
2020-03-04 10:28:14 +0100

    solucionando probremas con las rutas

--- server/aliscraper.js
+++ server/aliscraper.js
@@ -1,5 +1,5 @@
 const scrape = require('aliexpress-product-scraper-forker');
-const product = scrape('32788344921');
+const product = scrape('32720585644');
 
 product.then(res => {
   console.log('The JSON: ', res);
--- server/config/config.js
+++ server/config/config.js
@@ -4,4 +4,6 @@
 
 
 
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
+
+let UrlDb = 'mongodb+srv://lucusapp:romimu1111@cluster0-49zbz.mongodb.net/test?retryWrites=true&w=majority'
